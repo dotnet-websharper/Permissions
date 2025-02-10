5ea86dc25e67c3ca6d35ebd4c43c1a5a697489bc
Got <souvanxay38@gmail.com>
2025-02-10 11:25:17 +0100

    Add binding

---
+++ WebSharper.Permissions/Main.fs
@@ -0,0 +1,60 @@
+﻿namespace WebSharper.Permissions
+
+open WebSharper
+open WebSharper.JavaScript
+open WebSharper.InterfaceGenerator
+
+module Definition =
+
+    let PermissionState =
+        Pattern.EnumStrings "PermissionState" [
+            "granted"
+            "denied"
+            "prompt"
+        ]
+
+    let PermissionDescriptor =
+        Pattern.Config "PermissionDescriptor" {
+            Required = [
+                "name", T<string> 
+            ]
+            Optional = [
+                "userVisibleOnly", T<bool> 
+                "sysex", T<bool> 
+            ]
+        }
+
+    let PermissionStatus =
+        Class "PermissionStatus"
+        |=> Inherits T<Dom.EventTarget>
+        |+> Instance [
+            "name" =? T<string>
+            "state" =? PermissionState.Type
+
+            "onchange" =@ T<unit> ^-> T<unit>
+        ]
+
+    let Permissions =
+        Class "Permissions"
+        |+> Instance [
+            "query" => PermissionDescriptor?descriptor ^-> T<Promise<_>>[PermissionStatus.Type]
+        ]
+
+    let Assembly =
+        Assembly [
+            Namespace "WebSharper.Permissions" [
+                Permissions
+                PermissionStatus
+                PermissionState
+                PermissionDescriptor
+            ]
+        ]
+
+[<Sealed>]
+type Extension() =
+    interface IExtension with
+        member ext.Assembly =
+            Definition.Assembly
+
+[<assembly: Extension(typeof<Extension>)>]
+do ()
